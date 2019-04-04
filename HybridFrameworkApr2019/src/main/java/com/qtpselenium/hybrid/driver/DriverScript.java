3128c620bd2a296f4de9426f3dd09372cd87dd9d
Naveen <Naveen@192.168.0.17>
2019-04-03 23:09:01 -0300

    added

--- HybridFrameworkApr2019/src/main/java/com/qtpselenium/hybrid/driver/DriverScript.java
+++ HybridFrameworkApr2019/src/main/java/com/qtpselenium/hybrid/driver/DriverScript.java
@@ -3,6 +3,7 @@ package com.qtpselenium.hybrid.driver;
 import java.util.Hashtable;
 import java.util.Properties;
 
+import com.qtpselenium.hybrid.keywords.AppKeywords;
 import com.qtpselenium.hybrid.util.Xls_Reader;
 
 public class DriverScript {
@@ -10,7 +11,7 @@ public class DriverScript {
 	
 	public Properties envProp;
 	public Properties prop;
-	
+	AppKeywords app;
 
 
 
@@ -18,7 +19,12 @@ public void executeKeywords (String testName, Xls_Reader xls, Hashtable<String, 
 	int rows=xls.getRowCount("Keywords");
 System.out.println("num of rows "+ rows);
 
-
+app=new AppKeywords();
+//send properties to keywords class
+app.setEnvProp(envProp);
+app.setProp(prop);
+//send the hashtable data aswell to keywords class
+app.setData(data);
 for(int rNum=2;rNum<=rows;rNum++) {
 String tcid=xls.getCellData("Keywords", "TCID", rNum);
 if(tcid.equals(testName)) {
@@ -26,9 +32,29 @@ String keyword=xls.getCellData("Keywords", "Keyword", rNum);
 String objectkey=xls.getCellData("Keywords", "Object", rNum);
 String datakey=xls.getCellData("Keywords", "Data", rNum);
 String datavalue=data.get(datakey);
-System.out.println(tcid+"--"+keyword+"--"+objectkey+"--"+datakey+"--"+datavalue+"--");
-System.out.println("Object key has the value as : "+prop.getProperty(objectkey));
+//System.out.println(tcid+"--"+keyword+"--"+objectkey+"--"+datakey+"--"+datavalue+"--");
+//System.out.println("Object key has the value as : "+prop.getProperty(objectkey));
 
+
+app.setDatakey(datakey);
+app.setObjectkey(objectkey);
+
+
+if(keyword.equals("openBrowser")) {
+	app.openBrowser();
+}
+
+else if(keyword.equals("navigate")) {
+	app.navigate();
+}
+
+else if(keyword.equals("input")) {
+	app.type();
+}
+
+else if(keyword.equals("click")) {
+	app.click();
+}
 
 }
 }
--- HybridFrameworkApr2019/src/main/java/com/qtpselenium/hybrid/keywords/GenericKeywords.java
+++ HybridFrameworkApr2019/src/main/java/com/qtpselenium/hybrid/keywords/GenericKeywords.java
@@ -1,22 +1,76 @@
 package com.qtpselenium.hybrid.keywords;
 
-public class GenericKeywords {
+import java.util.Hashtable;
+import java.util.Properties;
 
+public class GenericKeywords {
+	
+	public Properties envProp;
+	public Properties prop;
+	public String objectkey;
+	public String datakey;
+	public Hashtable<String, String> data;
+		
+	/*******************Getter and setter***************************/
+
+	public Properties getEnvProp() {
+		return envProp;
+	}
+
+	public void setEnvProp(Properties envProp) {
+		this.envProp = envProp;
+	}
+
+	public Properties getProp() {
+		return prop;
+	}
+
+	public void setProp(Properties prop) {
+		this.prop = prop;
+	}
+
+	public String getObjectkey() {
+		return objectkey;
+	}
+
+	public void setObjectkey(String objectkey) {
+		this.objectkey = objectkey;
+	}
+
+	public String getDatakey() {
+		return datakey;
+	}
+
+	public void setDatakey(String datakey) {
+		this.datakey = datakey;
+	}
+
+	public Hashtable<String, String> getData() {
+		return data;
+	}
+
+	public void setData(Hashtable<String, String> data) {
+		this.data = data;
+	}
+
+	
+	/**********************************************/
 	public void openBrowser() {
+		System.out.println("open browser"+ data.get(datakey));
 		
 	}
 	
 	public void click() {
-		
+		System.out.println("click"+ prop.getProperty(objectkey));
 		
 	}
 	
 	
 	public void type() {
-		
+		System.out.println("type on object: "+ prop.getProperty(objectkey)+"   and value is"+data.get(datakey));
 	}
 	
 	public void navigate() {
-		
+		System.out.println("navigate"+ envProp.getProperty(objectkey));
 	}
 }
